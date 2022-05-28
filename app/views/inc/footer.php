0518b36a29433669742fcbf1aee98d950591c9b3
alnahian2003 <a.alnahian2003@gmail.com>
2022-05-28 22:40:07 +0600

    valid link src added

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -18,20 +18,19 @@
                 <div class="col-6 col-md-2 mb-3">
                     <h5>Get Started</h5>
                     <ul class="nav flex-column">
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Log In</a></li>
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Create Account</a></li>
+                        <li class="nav-item mb-2"><a href="<?= URL_ROOT; ?>/users/login" class="nav-link p-0 text-white">Log In</a></li>
+                        <li class="nav-item mb-2"><a href="<?= URL_ROOT; ?>/users/signup" class="nav-link p-0 text-white">Create Account</a></li>
                         <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Sign Out</a></li>
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Dashboard</a></li>
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Profile</a></li>
+                        <li class="nav-item mb-2"><a href="<?= URL_ROOT; ?>/users/profile" class="nav-link p-0 text-white">Profile</a></li>
                     </ul>
                 </div>
 
                 <div class="col-6 col-md-2 mb-3">
                     <h5>Legal</h5>
                     <ul class="nav flex-column">
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Privacy Policy</a></li>
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Terms & Conditions</a></li>
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Terms of Use</a></li>
+                        <li class="nav-item mb-2"><a href="<?= URL_ROOT; ?>/pages/privacy" class="nav-link p-0 text-white">Privacy Policy</a></li>
+                        <li class="nav-item mb-2"><a href="<?= URL_ROOT; ?>/pages/toc" class="nav-link p-0 text-white">Terms & Conditions</a></li>
+                        <li class="nav-item mb-2"><a href="<?= URL_ROOT; ?>/pages/tou" class="nav-link p-0 text-white">Terms of Use</a></li>
                     </ul>
                 </div>
 
