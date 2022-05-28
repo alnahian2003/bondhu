5cab874cf13a7c99b4c2306b0f38fca9bb7329be
alnahian2003 <a.alnahian2003@gmail.com>
2022-05-28 12:59:59 +0600

    Bootstrap 5.2 Configured with it's Icons CDN

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,6 @@
+<!-- Bootstrap 5.2 -->
+<script src="./js/bootstrap.bundle.min.js"></script>
+
 <!-- Main JavaScript -->
 <script src="<?= URL_ROOT; ?>/js/main.js"></script>
 
--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -6,6 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- Boostrap 5.2 CDN -->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+    <!-- Boostrap Icons CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+
     <!-- Main Stylesheet -->
     <link rel="stylesheet" href="<?= URL_ROOT; ?>/css/styles.css">
 
