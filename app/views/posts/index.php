325d4defb6e77e00d49b25b7f6d1d362284456ea
alnahian2003 <a.alnahian2003@gmail.com>
2022-05-30 21:44:37 +0600

    index page of posts set up done

---
+++ app/views/posts/index.php
@@ -0,0 +1,12 @@
+<?php
+// Site Title
+$site_title = "Posts";
+
+// Include Header
+require APP_ROOT . "/views/inc/header.php";
+?>
+
+<?php
+// Include Footer
+require APP_ROOT . "/views/inc/footer.php";
+?>
