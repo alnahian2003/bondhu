454ae61651421e6266bf6747197d153cd6d011e0
alnahian2003 <a.alnahian2003@gmail.com>
2022-05-30 22:06:57 +0600

    basic starting of posts feed created

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -6,6 +6,17 @@ $site_title = "Posts";
 require APP_ROOT . "/views/inc/header.php";
 ?>
 
+<!-- Posts heading section -->
+<div class="row align-items-center justify-content-between mb-2">
+    <div class="col-md-6 text-start">
+        <h1 class="h1">Newsfeed</h1>
+    </div>
+    <div class="col-md-6 text-end">
+        <a href="<?= URL_ROOT; ?>/posts/create" class="btn btn-primary">
+            <i class="bi bi-plus-circle"></i> Create Post
+        </a>
+    </div>
+</div>
 <?php
 // Include Footer
 require APP_ROOT . "/views/inc/footer.php";
